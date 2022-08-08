f1ae0762127a22e19c0ff42d08e04311784efff6
Omar Sayed <orhman@hotmail.com>
2022-08-08 19:53:39 +0200

    Add bootstrap, navbar, basic project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-
+// Import modules here for lazy loading
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
