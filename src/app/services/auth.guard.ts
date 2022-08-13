cf53d3c4fd2aed5556c842a42e2c8cd22d908c7c
Omar Sayed <orhman@hotmail.com>
2022-08-13 23:12:52 +0200

    Add dummy login/logout , guard on posts pages

---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,48 @@
+import { LoginLogoutService } from './login-logout.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private loginLogoutService: LoginLogoutService,
+    private router: Router,
+  ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.loginLogoutService.dummyAuth().then((isAuth: boolean) => {
+      if (isAuth) {
+        return true;
+      } else {
+        this.router.navigate(['/home']);
+        return false;
+      }
+    });
+  }
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.canActivate(childRoute, state);
+  }
+}
