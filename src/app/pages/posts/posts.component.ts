330e3eea2026cf94e8b094cdc18865b435ba38e2
Omar Sayed <orhman@hotmail.com>
2022-08-14 10:58:05 +0200

    Refactor routing to handle empty post child path

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -10,13 +10,14 @@ export class PostsComponent implements OnInit {
   constructor(private _router: Router, private _route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    //! No longer needed can be handled in routing module
     // to ensure the list is loaded on init
     // this is done to make the list and the detail sibling and only render one of them
-    this._router.navigate(['list'], { relativeTo: this._route });
-    this._router.events.subscribe((val: any) => {
-      if (val.url && val.url == '/posts') {
-        this._router.navigate(['posts/list']);
-      }
-    });
+    // this._router.navigate(['list'], { relativeTo: this._route });
+    // this._router.events.subscribe((val: any) => {
+    //   if (val.url && val.url == '/posts') {
+    //     this._router.navigate(['posts/list']);
+    //   }
+    // });
   }
 }
